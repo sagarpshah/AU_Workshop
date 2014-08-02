2693f98549577f1ffbd22816c99c149ab6b21241
Ishi Systems <ishi@Sagars-MacBook-Pro.local>
2014-08-02 07:59:11 +0530

    ScrollViews Demo

---
+++ ScrollViews/Step-1/ScrollViews/ScrollViews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollViews
+//
+//  Created by Ishi Systems on 02/08/14.
+//  Copyright (c) 2014 Ishi Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollViews/Step-1/ScrollViews/ScrollViews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollViews
+//
+//  Created by Ishi Systems on 02/08/14.
+//  Copyright (c) 2014 Ishi Systems. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
